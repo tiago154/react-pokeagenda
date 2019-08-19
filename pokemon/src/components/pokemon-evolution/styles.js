7c53d4a180958f83a0ea19307233435385759666
tiago154 <tiago.toya@hotmail.com>
2019-08-19 20:56:52 -0300

    Ajustando tela

--- pokemon/src/components/pokemon-evolution/styles.js
+++ pokemon/src/components/pokemon-evolution/styles.js
@@ -11,19 +11,14 @@ export const Container = styled.div`
     height: 38%;
 
     @media screen and (min-width: 1600px) {
-        height: 48%;
-    }
+        height: 48%
+    };
 
-    @media screen and (min-width: 768px) and (max-width: 1024px) {
-        height: 65%;
-    }
+    @media screen and (min-width: 768px) and (min-height: 1024px) {
+        height: 65%
+    };
 
-    @media screen and (min-width: 992px) and (max-width: 1200px) {
-        min-height: 170px;
-    }
-
-    @media screen and (min-width: 1201px) and (max-height: 700px){
-        margin-top: 10px;
-        height: 240px;
-    } */
+    @media screen and (min-width: 1600px) and (min-height: 900px) {
+        height: 56%
+    };
 `;
