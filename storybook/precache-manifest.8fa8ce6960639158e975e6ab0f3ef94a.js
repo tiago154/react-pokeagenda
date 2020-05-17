1bf6f442a82edb66eaf7e3ce2fd6f45aa8c98592
tiago154 <tiago.toya@hotmail.com>
2020-05-17 18:35:56 +0000

    Deploying to master from  @ a09961eb07839953566649685ca57af6c433241d 🚀

---
+++ storybook/precache-manifest.8fa8ce6960639158e975e6ab0f3ef94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4e5f6fe6fdfb912bdae3aa6536e663",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.db4d3b48f541530537c4.bundle.js"
+  },
+  {
+    "url": "runtime~main.db4d3b48f541530537c4.bundle.js"
+  },
+  {
+    "revision": "fe61c7db3246a579dea1ad7d91a91a8e",
+    "url": "static/media/pikachu-running.fe61c7db.gif"
+  },
+  {
+    "revision": "f97f5c6510994f677877b08320475008",
+    "url": "static/media/pokeball-catching.f97f5c65.gif"
+  },
+  {
+    "url": "vendors~main.db4d3b48f541530537c4.bundle.js"
+  },
+  {
+    "url": "vendors~main.db4d3b48f541530537c4.bundle.js.LICENSE"
+  }
+]);
