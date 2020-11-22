1d252573f5ce2ab934cede7f409f9ba62548e42d
Tiago Silva <tiago.toya@hotmail.com>
2020-11-22 12:08:17 -0300

    docs: ajustando storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,2 @@
 import '@storybook/addon-knobs/register'
-import '@storybook/addon-links/register';
+import '@storybook/addon-links/register'
--- src/components/PokemonRow/index.stories.js
+++ src/components/PokemonRow/index.stories.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import PokemonRow from '.'
-import { withKnobs, select } from '@storybook/addon-knobs'
+import { withKnobs, text, number } from '@storybook/addon-knobs'
 
 export default {
   title: 'PokemonRow',
   decorators: [withKnobs]
 }
 
-const ids = [7, 25, 36, 151]
-const names = ['Squirtle', 'Pikachu', 'Clefable', 'Mew']
-const spriteUrls = [
-  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ids[0]}.png`,
-  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ids[1]}.png`,
-  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ids[2]}.png`,
-  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ids[3]}.png`
-]
-
 export const Linha = () =>
   <PokemonRow
-    id={select('id', ids, ids[0])}
-    name={select('name', names, names[0])}
-    spriteUrl={select('spriteUrl', spriteUrls, spriteUrls[0])}
-  >
-  </PokemonRow>
+    pokemon={
+      {
+        id: number('id', 7),
+        name: text('name', 'Squirtle'),
+        image: text('image', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png')
+      }
+    }
+  />
